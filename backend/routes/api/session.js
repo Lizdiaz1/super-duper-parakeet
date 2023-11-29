21707025cebbda15f49fe52d8da01e2cd44dab55
Martha Diaz <mdlizette1@gmail.com>
2023-11-29 18:22:01 -0500

    Add validation for login inputs

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,13 +2,25 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { setTokenCookie } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
 // Log in
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.unscoped().findOne({
@@ -37,16 +49,13 @@ router.post('/', async (req, res, next) => {
     await setTokenCookie(res, safeUser);
 
     return res.json({ user: safeUser });
-  });
+});
 
 // Log out
-router.delete(
-  '/',
-  (_req, res) => {
+router.delete('/', (_req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });
-  }
-);
+});
 
 // Restore session user
 router.get('/', (req, res) => {
---
+++ backend/utils/validation.js
@@ -0,0 +1,23 @@
+// backend/utils/validation.js
+const { validationResult } = require('express-validator');
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors.array().forEach((error) => {
+      errors[error.param] = error.msg;
+    });
+
+    const err = new Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  } else {
+    next();
+  }
+};
+
+module.exports = { handleValidationErrors };
