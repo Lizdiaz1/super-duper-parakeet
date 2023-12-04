b40bb942b1e3b9c5153e151adc81fe624e1ec996
Martha Diaz <mdlizette1@gmail.com>
2023-12-03 22:54:58 -0500

    login a user

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,10 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { ValidationError } = require('sequelize');
-
+const sessionRouter = require('./routes/api/session');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
-
 
 const routes = require('./routes');
 
@@ -44,6 +43,7 @@ if (!isProduction) {
   );
 
   app.use(routes);
+  app.use('/api/session', sessionRouter);
 
   // Catch unhandled requests and forward to error handler
 app.use((_req, _res, next) => {
