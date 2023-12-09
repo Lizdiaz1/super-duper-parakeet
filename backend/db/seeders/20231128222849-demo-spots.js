51dadf1d745fbf8aa61a7281832579de9ab813a4
Martha Diaz <mdlizette1@gmail.com>
2023-12-09 16:36:24 -0500

    removed validate

--- backend/db/seeders/20231128222849-demo-spots.js
+++ backend/db/seeders/20231128222849-demo-spots.js
@@ -22,9 +22,9 @@ module.exports = {
 					lng: 7.74912,
 					name: "Luxurious Swiss Chalet",
 					description: "This chalet includes five deluxe bedroom suites, open-plan living spaces with floor-to-ceiling windows, a private in-house Gourmet Chef, and a carefully curated art collection.",
-					price: 48285,
-					createdAt: new Date(),
-					updatedAt: new Date()
+					price: 48285
+				//	createdAt: new Date(),
+				//	updatedAt: new Date()
 				},
 				{
 					ownerId: 1,
@@ -36,9 +36,9 @@ module.exports = {
 					lng: 22.22,
 					name: "The Wardrobe",
 					description: "It's bigger on the inside",
-					price: 50.00,
-					createdAt: new Date(),
-					updatedAt: new Date()
+					price: 50.00
+				//	createdAt: new Date(),
+				//	updatedAt: new Date()
 				},
 				{
 					ownerId: 2,
@@ -50,9 +50,9 @@ module.exports = {
 					lng: -76.38200,
 					name: "Cove Point Lighthouse Keeper's House",
 					description: "This historic site, listed on the National Register of Historic Places, includes accommodation for up to eight guests in three bedrooms. It's an active lighthouse and keeper’s home located on a seven-acre point of land in Chesapeake Bay. The cottage features a screened-in porch with views of the lighthouse and water, and guests have access to a beach. Modern comforts like a full kitchen, laundry room, Wi-Fi, and TV are included",
-					price: 325,
-					createdAt: new Date(),
-					updatedAt: new Date()
+					price: 325
+				//	createdAt: new Date(),
+				//	updatedAt: new Date()
 				},
 				{
 					ownerId: 3,
@@ -64,12 +64,12 @@ module.exports = {
 					lng: 35.4730327,
 					name: "Disney Land",
 					description: "It's the entire Disney Land Park. Rent out the whole park",
-					price: 350000,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				},
-			],
-			{ validate: true }
+					price: 350000
+					//createdAt: new Date(),
+					//updatedAt: new Date() testing the errors
+				}
+			]
+
 		);
 	},
 
@@ -81,7 +81,7 @@ module.exports = {
 			{
 				name: { [Op.in]: ["Disney Land",
 				"Cove Point Lighthouse Keeper's House",
-				"The Wardrobe", "Luxurious Swiss Chalet"] },
+				"The Wardrobe", "Luxurious Swiss Chalet"] }
 			}, 	{});
-	},
+	}
 };
